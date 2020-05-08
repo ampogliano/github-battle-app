d03d0e2d5b02216e68161bdf8043d2c4463e8362
Anthony Pogliano <poglianoster@gmail.com>
2020-05-07 21:36:21 -0600

    Added theming for light/dark theme.

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Hover from './Hover'
+import Hover from './Hover';
+import { ThemeConsumer } from '../contexts/theme';
 
 const styles = {
   container: {
@@ -21,6 +22,21 @@ const styles = {
     color: '#fff',
     textAlign: 'center',
     fontSize: '14px'
+  },
+  tooltipDark: {
+    boxSizing: 'border-box',
+    position: 'absolute',
+    width: '160px',
+    bottom: '100%',
+    left: '50%',
+    marginLeft: '-80px',
+    borderRadius: '3px',
+    padding: '7px',
+    marginBottom: '5px',
+    textAlign: 'center',
+    fontSize: '14px',
+    backgroundColor: 'hsla(0, 0%, 85%, 0.9)',
+    color: '#000'
   }
 }
 
@@ -28,10 +44,15 @@ export default function Tooltip({ text, children }) {
   return (
     <Hover>
       {(hovering) => (
-        <div style={styles.container}>
-          {hovering === true && <div style={styles.tooltip}>{text}</div>}
-          {children}
-        </div>
+        <ThemeConsumer>
+          {({ theme }) => (
+            <div style={styles.container}>
+              {hovering === true && <div style={theme === 'dark' ? styles.tooltipDark : styles.tooltip}>{text}</div>}
+              {children}
+            </div>
+          )}
+        </ThemeConsumer>
+
       )}
     </Hover>
   )
