7da5be264e476ce63fde1626d2673f6c52652d10
Anthony Pogliano <poglianoster@gmail.com>
2020-05-14 11:55:24 -0600

    I don't know how to successfully manage many things in life... especially not merge conflicts.

--- app/components/Routes.js
+++ app/components/Routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import Nav from './Nav';
@@ -8,15 +7,6 @@ const Popular = React.lazy(() => import('./Popular'));
 const Battle = React.lazy(() => import('./Battle'));
 const Results = React.lazy(() => import('./Results'));
 const NotFound = React.lazy(() => import('./NotFound'));
-=======
-import React from 'react';
-import Nav from './Nav';
-import Popular from './Popular';
-import Battle from './Battle';
-import Results from './Results';
-import NotFound from './NotFound';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
->>>>>>> 12edb16906a05af8523834823e961ca121083c31
 
 
 
@@ -26,7 +16,6 @@ export default class Routes extends React.Component {
       <Router>
         <div className='container'>
           <Nav />
-<<<<<<< HEAD
           <React.Suspense fallback={<Loading />}>
             <Switch>
               <Route exact path='/' component={Popular} />
@@ -35,14 +24,6 @@ export default class Routes extends React.Component {
               <Route component={NotFound} />
             </Switch>
           </React.Suspense>
-=======
-          <Switch>
-            <Route exact path='/' component={Popular} />
-            <Route exact path='/battle' component={Battle} />
-            <Route path='/battle/results' component={Results} />
-            <Route component={NotFound} />
-          </Switch>
->>>>>>> 12edb16906a05af8523834823e961ca121083c31
         </div>
       </Router>
     )
